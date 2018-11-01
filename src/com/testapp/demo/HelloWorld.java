0a6bbb8c109a334d10d1cd0baf9d437e26e9c317
keepcccccc <keepcccccc@outlook.com>
2018-11-01 19:00:19 +0800

    学习java

--- src/com/testapp/demo/HelloWorld.java
+++ src/com/testapp/demo/HelloWorld.java
@@ -5,6 +5,6 @@ package com.testapp.demo;
  */
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World"); // 打印HelloWorld
     }
 }
