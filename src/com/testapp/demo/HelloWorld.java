5b853e633ce7c8d49d431d5fb8939e2017cc1b90
keepcccccc <keepcccccc@outlook.com>
2018-11-01 18:23:00 +0800

    Initial commit

---
+++ src/com/testapp/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.testapp.demo;
+
+/**
+ * Created by king on 2018/11/1.
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
